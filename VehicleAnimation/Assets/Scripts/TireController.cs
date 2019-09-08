0ada7b1ff270469abd381bcb9406eb5d231db4cf
jackle1127 <nanotek2711@yahoo.com>
2019-09-08 17:14:37 -0400

    Initial commit

---
+++ VehicleAnimation/Assets/Scripts/TireController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TireController : MonoBehaviour
+{
+    [SerializeField] private Transform _tireChild;
+    [SerializeField] private bool _frontTire = true;
+    [SerializeField] private Transform _targetRotation;
+    [SerializeField] private float _radius;
+    private Vector3 _prevPos = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _prevPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_frontTire && _targetRotation)
+        {
+            Vector3 upVector = Vector3.Cross(transform.forward, _targetRotation.right);
+            Vector3 forwardVector = Vector3.Cross(_targetRotation.right, upVector);
+            transform.rotation = Quaternion.LookRotation(forwardVector, upVector);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.identity; // Reset the child orientation so that the forward of the tire parent is forward of the vehicle.
+        }
+        if (_tireChild)
+        {
+            Vector3 delta = transform.position - _prevPos;
+            delta = Vector3.ProjectOnPlane(delta, transform.right);
+            float distance = delta.magnitude;
+            if (Vector3.Dot(delta, transform.forward) < 0)
+            {
+                distance = -distance;
+            }
+            float deltaAngle = 360f * distance / (2 * Mathf.PI * _radius);
+            _tireChild.localRotation = Quaternion.AngleAxis(deltaAngle, Vector3.left) * _tireChild.localRotation;
+        }
+        _prevPos = transform.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        const float segments = 60;
+        const float deltaAngle = 360 / segments;
+        for (float deg = 0; deg <= 360 - deltaAngle; deg += deltaAngle)
+        {
+            Vector3 point1 = Vector3.up * _radius;
+            point1 = Quaternion.AngleAxis(deg, transform.right) * point1;
+            Vector3 point2 = Quaternion.AngleAxis(deltaAngle, transform.right) * point1;
+            point1 += transform.position;
+            point2 += transform.position;
+            Gizmos.DrawLine(point1, point2);
+        }
+    }
+}
---
+++ VehicleAnimation/Assets/Scripts/VehicleControlPointController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleControlPointController : MonoBehaviour
+{
+    private const float DISTANCE_THRESHOLD = .01f;
+
+    [SerializeField] private Transform _leadingPoint;
+    [SerializeField] private Transform _controllingTransform;
+    private float _distanceToLeadingPoint;
+    private Matrix4x4 _controllingTransformDelta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _distanceToLeadingPoint = (_leadingPoint.position - transform.position).magnitude;
+        if (_controllingTransform)
+        {
+            _controllingTransformDelta = transform.worldToLocalMatrix * _controllingTransform.localToWorldMatrix;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentDistance = (_leadingPoint.position - transform.position).magnitude;
+        float deltaDistance = currentDistance - _distanceToLeadingPoint;
+        if (Mathf.Abs(deltaDistance) >= DISTANCE_THRESHOLD)
+        {
+            Vector3 toLeadingPoint = (_leadingPoint.position - transform.position).normalized;
+            transform.rotation = Quaternion.LookRotation(toLeadingPoint, Vector3.up);
+            transform.position += toLeadingPoint * deltaDistance;
+        }
+        if (_controllingTransform)
+        {
+            Matrix4x4 controllingPosition = transform.localToWorldMatrix * _controllingTransformDelta;
+            _controllingTransform.position = controllingPosition.MultiplyPoint(Vector3.zero);
+            _controllingTransform.rotation = controllingPosition.rotation;
+        }
+    }
+}
